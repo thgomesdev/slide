ad37b8833a70de43bfd008522c93be593926a2d4
Thiago Gomes <thiagogomesabreu01@gmail.com>
2023-03-20 23:21:47 -0300

    calculo de distancias

--- js/slide.js
+++ js/slide.js
@@ -2,17 +2,33 @@ export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
-  onMove(event) {}
+  moveSlide(distX) {
+    this.dist.lastPosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.5;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onEnd(event) {
     this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.lastPosition;
   }
 
   addSlideEvents() {
